856de9f3b03a2fae2adf9d25e46f30fcb999d56d
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-22 01:17:46 +0200

    active class update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
               <li key={index}>
                 <NavLink
                   to={path}
+                  end
                   className={({ isActive }) =>
                     isActive ? 'active-nav' : ''
                   }
