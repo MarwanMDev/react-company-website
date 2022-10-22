65121c64e247b4a21a0f0b7e4c088ab4647106b1
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-22 21:02:04 +0200

    Programs Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './home.css';
 import MainHeader from '../../components/MainHeader';
+import Programs from '../../components/Programs';
 
 const Home = () => {
   return (
     <>
       <MainHeader />
+      <Programs />
     </>
   );
 };
