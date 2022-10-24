0cc10c96204f4a355c46b51c4e4833387113c3a0
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-24 22:27:26 +0200

    Adding section to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,9 @@
 import './about.css';
 import Header from '../../components/Header';
 import Image from '../../images/header_bg_1.jpg';
+import StoryImage from '../../images/about1.jpg';
+import VisionImage from '../../images/about2.jpg';
+import MissionImage from '../../images/about3.jpg';
 
 const About = () => {
   return (
@@ -11,6 +14,82 @@ const About = () => {
         nihil, dolores id ipsa, hic delectus eos nobis ad voluptas.
         Esse a quis quaerat voluptatibus.
       </Header>
+
+      <section className="about__kstory">
+        <div className="container about__story-container">
+          <div className="about__section-image">
+            <img src={StoryImage} alt="StoryImage" />
+          </div>
+          <div className="about__section-content">
+            <h1>Our Story</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Ullam quas non sint debitis fugiat rem, nisi
+              inventore perferendis dicta nesciunt laudantium alias
+              architecto voluptatum deleniti corporis quod placeat vel
+              eligendi.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Asperiores facilis dicta quo ipsam architecto,
+              nesciunt perferendis exercitationem veniam est ipsum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quaerat numquam illum eaque.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="about__kstory">
+        <div className="container about__story-container">
+          <div className="about__section-content">
+            <h1>Our Vision</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Ullam quas non sint debitis fugiat rem, nisi
+              inventore perferendis dicta nesciunt laudantium alias
+              architecto voluptatum deleniti corporis quod placeat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Asperiores facilis dicta quo ipsam architecto,
+              nesciunt perferendis exercitationem veniam est ipsum.
+            </p>
+          </div>
+          <div className="about__section-image">
+            <img src={VisionImage} alt="VisionImage" />
+          </div>
+        </div>
+      </section>
+
+      <section className="about__kstory">
+        <div className="container about__story-container">
+          <div className="about__section-image">
+            <img src={MissionImage} alt="MissionImage" />
+          </div>
+          <div className="about__section-content">
+            <h1>Our Mission</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Ullam quas non sint debitis fugiat rem, nisi
+              inventore perferendis dicta nesciunt laudantium alias
+              architecto voluptatum deleniti corporis quod placeat vel
+              eligendi.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Asperiores facilis dicta quo ipsam architecto,
+              nesciunt perferendis exercitationem veniam est ipsum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quaerat numquam illum eaque.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
