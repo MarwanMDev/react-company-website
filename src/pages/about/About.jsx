ade1042336c8e1fa84693439dd9b210b67a3ca1d
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-24 22:07:15 +0200

    About Us Page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
 import './about.css';
+import Header from '../../components/Header';
+import Image from '../../images/header_bg_1.jpg';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Header title="About Us" image={Image}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aliquam necessitatibus quia suscipit, vero veritatis est
+        nihil, dolores id ipsa, hic delectus eos nobis ad voluptas.
+        Esse a quis quaerat voluptatibus.
+      </Header>
+    </>
+  );
 };
 
 export default About;
