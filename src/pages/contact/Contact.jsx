b9ea1e69ec852caa21f7141cd748bef2826c0829
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-26 22:43:38 +0200

    contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_2.jpg';
+import { MdEmail } from 'react-icons/md';
+import { BsMessenger } from 'react-icons/bs';
+import { IoLogoWhatsapp } from 'react-icons/io';
 import './contact.css';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Harum minus labore voluptatum id facere delectus quo.
+      </Header>
+
+      <section className="contact">
+        <div className="container contact_container">
+          <div className="contact_wrapper">
+            <a
+              href="mailto:support@application.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="http://m.me/marwan_mostafa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsMessenger />
+            </a>
+            <a
+              href="https://wa.me/+20100832217"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
