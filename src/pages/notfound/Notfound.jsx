6ba7bec79d220ec21f9327f4c3e5e491c95d17ba
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-10-27 19:03:59 +0200

    Not found page

--- src/pages/notfound/Notfound.jsx
+++ src/pages/notfound/Notfound.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './notfound.css';
 
 const Notfound = () => {
-  return <div>Notfound</div>;
+  return (
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Notfound;
